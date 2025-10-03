a5ea5bf5aef9c6aa14ffd1caca59dd86528edb5a
Adam Bloom <adam.bloom2012@gmail.com>
2025-10-03 16:13:00 -0400

    chore: new submit scripts

---
+++ satellite_model/submit_monthly_gpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#BSUB -n 2
+#BSUB -W 24:00
+#BSUB -q gpu
+#BSUB -gpu "num=1:mode=shared:mps=yes"
+#BSUB -R "select[a30 || a10 || l40 || a100 || h100 ]"
+#BSUB -R "rusage[mem=72GB]"
+#BSUB -J train_sat
+#BSUB -o logs/out.%J
+#BSUB -e logs/err.%J
+
+source ~/.bashrc
+source /usr/local/usrapps/atmoschem/abloom/venv/bin/activate #pred has extra packages
+
+python training.py --epochs=20 --datadir=/share/atmoschem/abloom/data/eea --samples_file="../data/samples_S2S5P_monthly_eea.csv"
