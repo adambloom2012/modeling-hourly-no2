b7efa1cb0586bd4a423574f4d41a80f0d29c976c
adambloom2012 <adam.bloom2012@gmail.com>
2025-10-20 11:23:57 -0400

    chore: fix in test pipeline

--- satellite_model/train_utils.py
+++ satellite_model/train_utils.py
@@ -1,28 +1,24 @@
+from model import get_model
+from transforms import ChangeBandOrder, ToTensor, DatasetStatistics, Normalize, Randomize
+from dataset import NO2PredictionDataset
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch import nn, optim
+import torch
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import copy
+import sys
 import os
 
-os.environ["OMP_NUM_THREADS"] = "6" # export OMP_NUM_THREADS=4
-os.environ["OPENBLAS_NUM_THREADS"] = "6" # export OPENBLAS_NUM_THREADS=4
-os.environ["MKL_NUM_THREADS"] = "6" # export MKL_NUM_THREADS=6
-os.environ["VECLIB_MAXIMUM_THREADS"] = "6" # export VECLIB_MAXIMUM_THREADS=4
-os.environ["NUMEXPR_NUM_THREADS"] = "6" # export NUMEXPR_NUM_THREADS=6
-
-import sys
-import copy
-from tqdm  import tqdm
-
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
-
-import torch
-from torch import nn, optim
-from torch.utils.data import DataLoader
-from torchvision import transforms
-
-from dataset import NO2PredictionDataset
-from transforms import ChangeBandOrder, ToTensor, DatasetStatistics, Normalize, Randomize
-from model import get_model
+os.environ["OMP_NUM_THREADS"] = "6"  # export OMP_NUM_THREADS=4
+os.environ["OPENBLAS_NUM_THREADS"] = "6"  # export OPENBLAS_NUM_THREADS=4
+os.environ["MKL_NUM_THREADS"] = "6"  # export MKL_NUM_THREADS=6
+os.environ["VECLIB_MAXIMUM_THREADS"] = "6"  # export VECLIB_MAXIMUM_THREADS=4
+os.environ["NUMEXPR_NUM_THREADS"] = "6"  # export NUMEXPR_NUM_THREADS=6
 
 
 def eval_metrics(y, y_hat):
@@ -32,15 +28,21 @@ def eval_metrics(y, y_hat):
 
     return [r2, mae, mse]
 
+
 def split_samples(samples, stations, test_size=0.2, val_size=0.2):
-    stations_train, stations_test = train_test_split(stations, test_size=test_size)
+    stations_train, stations_test = train_test_split(
+        stations, test_size=test_size)
     real_val_size = val_size / (1 - test_size)
-    stations_train, stations_val = map(set, train_test_split(stations_train, test_size=real_val_size))
+    stations_train, stations_val = map(
+        set, train_test_split(stations_train, test_size=real_val_size))
     stations_test = set(stations_test)
 
-    samples_train = [s for s in samples if s["AirQualityStation"] in stations_train]
-    samples_test = [s for s in samples if s["AirQualityStation"] in stations_test]
-    samples_val = [s for s in samples if s["AirQualityStation"] in stations_val]
+    samples_train = [
+        s for s in samples if s["AirQualityStation"] in stations_train]
+    samples_test = [
+        s for s in samples if s["AirQualityStation"] in stations_test]
+    samples_val = [
+        s for s in samples if s["AirQualityStation"] in stations_val]
 
     # samples_train = copy.deepcopy([s for s in samples if s["AirQualityStation"] in stations_train])
     # samples_test = copy.deepcopy([s for s in samples if s["AirQualityStation"] in stations_test])
@@ -51,15 +53,14 @@ def split_samples(samples, stations, test_size=0.2, val_size=0.2):
     # samples_val = []
 
     # while samples:
-        # sample = samples.pop()
-        # station = sample["AirQualityStation"]
-        # if station in stations_train:
-            # samples_train.append(copy.deepcopy(sample))
-        # elif station in stations_test:
-            # samples_test.append(copy.deepcopy(sample))
-        # elif station in stations_val:
-            # samples_val.append(copy.deepcopy(sample))
-
+    # sample = samples.pop()
+    # station = sample["AirQualityStation"]
+    # if station in stations_train:
+    # samples_train.append(copy.deepcopy(sample))
+    # elif station in stations_test:
+    # samples_test.append(copy.deepcopy(sample))
+    # elif station in stations_val:
+    # samples_val.append(copy.deepcopy(sample))
 
     return samples_train, samples_val, samples_test, stations_train, stations_val, stations_test
 
@@ -68,9 +69,11 @@ def split_samples_df(samples, test_size=0.2, val_size=0.2):
     """split pd.DF s.t. all samples of a given station
     are either in the train or test set """
     stations = samples.AirQualityStation.unique()
-    stations_train, stations_test = train_test_split(stations, test_size=test_size)
+    stations_train, stations_test = train_test_split(
+        stations, test_size=test_size)
     real_val_size = val_size / (1 - test_size)
-    stations_train, stations_val = map(set, train_test_split(stations_train, test_size=real_val_size))
+    stations_train, stations_val = map(
+        set, train_test_split(stations_train, test_size=real_val_size))
     stations_test = set(stations_test)
 
     samples_train = samples[samples.AirQualityStation.isin(stations_train)]
@@ -78,6 +81,7 @@ def split_samples_df(samples, test_size=0.2, val_size=0.2):
     samples_test = samples[samples.AirQualityStation.isin(stations_test)]
 
     return samples_train, samples_val, samples_test
+
 
 def train(sources, model, loss, optimizer, scheduler, dataloader, epochs, device, dropout, heteroscedastic):
     model.train()
@@ -93,7 +97,7 @@ def train(sources, model, loss, optimizer, scheduler, dataloader, epochs, device
                 img = sample["img"].float().to(device)
                 s5p = sample["s5p"].float().unsqueeze(dim=1).to(device)
                 y = sample["no2"].float().to(device)
-                model_input = {"img" : img, "s5p" : s5p}
+                model_input = {"img": img, "s5p": s5p}
                 y_hat = model(model_input).squeeze()
                 loss_epoch = loss(y_hat, y)
                 loss_epoch += loss_epoch.item()
@@ -111,7 +115,7 @@ def train(sources, model, loss, optimizer, scheduler, dataloader, epochs, device
                 optimizer.step()
             scheduler.step(epoch)
             torch.cuda.empty_cache()
-            loss_history.append(loss_epoch/idx) # save the epochs average loss
+            loss_history.append(loss_epoch/idx)  # save the epochs average loss
 #            print("Epoch", epoch, "loss:", loss_epoch.cpu().detach().numpy().item()/idx)
     else:
         for epoch in range(epochs):
@@ -127,10 +131,11 @@ def train(sources, model, loss, optimizer, scheduler, dataloader, epochs, device
                 optimizer.step()
             scheduler.step(epoch)
             torch.cuda.empty_cache()
-            loss_history.append(loss_epoch/idx) # save the epochs average loss
+            loss_history.append(loss_epoch/idx)  # save the epochs average loss
 #            print("Epoch", epoch, "loss:", loss_epoch.cpu().detach().numpy().item()/idx)
 
     return model
+
 
 def test(sources, model, dataloader, device, datastats, dropout, heteroscedastic):
     # test
@@ -145,8 +150,9 @@ def test(sources, model, dataloader, device, datastats, dropout, heteroscedastic
             for idx, sample in enumerate(dataloader):
                 img = sample["img"].float().to(device)
                 s5p = sample["s5p"].float().unsqueeze(dim=1).to(device)
+                hour = sample["hour"].float().to(device)
                 y = sample["no2"].float().to(device).squeeze()
-                model_input = {"img" : img, "s5p" : s5p}
+                model_input = {"img": img, "s5p": s5p, "hour": hour}
                 y_hat = model(model_input).squeeze()
 
                 if heteroscedastic:
@@ -184,6 +190,7 @@ def test(sources, model, dataloader, device, datastats, dropout, heteroscedastic
 
     return measurements, predictions
 
+
 if __name__ == "__main__":
     from utils import load_data
     # call as `python train.py data/samples_S2_whole_timespan.csv /netscratch/lscheibenreif/eea`
@@ -196,11 +203,12 @@ if __name__ == "__main__":
     else:
         frequency = samples_file.split("_")[2].replace(".csv", "")
 
-    if verbose: print("Loading data...")
+    if verbose:
+        print("Loading data...")
     samples, stations = load_data(datadir, samples_file, frequency, sources)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    #if verbose:
+    # if verbose:
     #    print("device:", device)
     #    print("frequency:", frequency)
     #    print("sources:", sources)
@@ -213,7 +221,8 @@ if __name__ == "__main__":
     epochs = 20
     batch_size = 50
     datastats = DatasetStatistics()
-    tf = transforms.Compose([ChangeBandOrder(), Normalize(datastats), Randomize(), ToTensor()])
+    tf = transforms.Compose(
+        [ChangeBandOrder(), Normalize(datastats), Randomize(), ToTensor()])
 
     num_runs = 20
     performances_test = []
@@ -221,21 +230,29 @@ if __name__ == "__main__":
     performances_train = []
     for run in tqdm(range(num_runs)):
 
-        samples_train, samples_val, samples_test = split_samples(samples, stations, 0.2, 0.2)
+        samples_train, samples_val, samples_test = split_samples(
+            samples, stations, 0.2, 0.2)
         if verbose:
             print("run:", run)
             print("train size:", len(samples_train))
             print("test size:", len(samples_test))
             print("val size:", len(samples_val))
 
-        dataset_test = NO2PredictionDataset(datadir, samples_test, frequency, sources, transforms=tf)
-        dataset_train = NO2PredictionDataset(datadir, samples_train, frequency, sources, transforms=tf)
-        dataset_val = NO2PredictionDataset(datadir, samples_val, frequency, sources, transforms=tf)
+        dataset_test = NO2PredictionDataset(
+            datadir, samples_test, frequency, sources, transforms=tf)
+        dataset_train = NO2PredictionDataset(
+            datadir, samples_train, frequency, sources, transforms=tf)
+        dataset_val = NO2PredictionDataset(
+            datadir, samples_val, frequency, sources, transforms=tf)
 
-        dataloader_train = DataLoader(dataset_train, batch_size=batch_size, num_workers=4, shuffle=True, pin_memory=True)
-        dataloader_test = DataLoader(dataset_test, batch_size=1, num_workers=1, shuffle=False, pin_memory=True)
-        dataloader_val = DataLoader(dataset_val, batch_size=1, num_workers=1, shuffle=False, pin_memory=True)
-        dataloader_train_for_testing = DataLoader(dataset_train, batch_size=1, num_workers=1, shuffle=False, pin_memory=True)
+        dataloader_train = DataLoader(
+            dataset_train, batch_size=batch_size, num_workers=4, shuffle=True, pin_memory=True)
+        dataloader_test = DataLoader(
+            dataset_test, batch_size=1, num_workers=1, shuffle=False, pin_memory=True)
+        dataloader_val = DataLoader(
+            dataset_val, batch_size=1, num_workers=1, shuffle=False, pin_memory=True)
+        dataloader_train_for_testing = DataLoader(
+            dataset_train, batch_size=1, num_workers=1, shuffle=False, pin_memory=True)
 
         model = get_model(sources, device, checkpoint)
         model.to(device)
@@ -245,12 +262,15 @@ if __name__ == "__main__":
 
         # optimize entire model (head + backbone)
         optimizer = optim.SGD(model.parameters(), lr=0.001)
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.5, threshold=1e-5, min_lr=1e-6)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, 'min', factor=0.5, threshold=1e-5, min_lr=1e-6)
 
         for epoch in range(epochs):
 
-            model = train(sources, model, loss, optimizer, scheduler, dataloader_train, 1, device)
-            val_y, val_y_hat = test(sources, model, dataloader_val, device, datastats)
+            model = train(sources, model, loss, optimizer,
+                          scheduler, dataloader_train, 1, device)
+            val_y, val_y_hat = test(
+                sources, model, dataloader_val, device, datastats)
 
             valid_val = (val_y < 100) & (val_y > 0)
 
@@ -259,13 +279,16 @@ if __name__ == "__main__":
             if epoch > 5 and sum(valid_val) > len(valid_val) - 5:
                 if eval_val[1] > np.mean([performances_val[-3][3], performances_val[-2][3], performances_val[-1][3]]):
                     # performance on evaluation set is decreasing
-                    if verbose: print("Stop at epoch:", epoch)
+                    if verbose:
+                        print("Stop at epoch:", epoch)
                     break
 
             performances_val.append([run, epoch] + eval_val)
 
-        test_y, test_y_hat = test(sources, model, dataloader_test, device, datastats)
-        train_y, train_y_hat = test(sources, model, dataloader_train_for_testing, device, datastats)
+        test_y, test_y_hat = test(
+            sources, model, dataloader_test, device, datastats)
+        train_y, train_y_hat = test(
+            sources, model, dataloader_train_for_testing, device, datastats)
 
         valid = (test_y < 100) & (test_y > 0)
         valid_train = (train_y < 100) & (train_y > 0)
@@ -281,13 +304,18 @@ if __name__ == "__main__":
             print("\tvalid sum,len:", sum(valid), len(valid))
             print("\ttrain scores:", *eval_train)
             print("\ttest scores:", *eval_test)
-            #print("\tval scores:", *eval_val)
+            # print("\tval scores:", *eval_val)
 
-    performances_train = pd.DataFrame(performances_train, columns=["r2", "mae", "mse"])
-    performances_test = pd.DataFrame(performances_test, columns=["r2", "mae", "mse"])
-    performances_val = pd.DataFrame(performances_val, columns=["run", "epoch", "r2", "mae", "mse"])
+    performances_train = pd.DataFrame(
+        performances_train, columns=["r2", "mae", "mse"])
+    performances_test = pd.DataFrame(
+        performances_test, columns=["r2", "mae", "mse"])
+    performances_val = pd.DataFrame(performances_val, columns=[
+                                    "run", "epoch", "r2", "mae", "mse"])
 
-    performances_train.to_csv("data/performances_train_" + sources + "_" + frequency + ".csv", index=False)
-    performances_test.to_csv("data/performances_test_" + sources + "_" + frequency + ".csv", index=False)
-    performances_val.to_csv("data/performances_val_" + sources + "_" + frequency + ".csv", index=False)
-
+    performances_train.to_csv(
+        "data/performances_train_" + sources + "_" + frequency + ".csv", index=False)
+    performances_test.to_csv(
+        "data/performances_test_" + sources + "_" + frequency + ".csv", index=False)
+    performances_val.to_csv("data/performances_val_" +
+                            sources + "_" + frequency + ".csv", index=False)
